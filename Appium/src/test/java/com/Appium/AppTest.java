844bd87fc5fb56037b0422ade053cd84d68b35a1
Kanchan <kanchanpatil9035@gmail.com>
2022-12-04 13:45:42 +0530

    Uploading All Files

---
+++ Appium/src/test/java/com/Appium/AppTest.java
@@ -0,0 +1,18 @@
+package com.Appium;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        System.out.println("tESTED APP");
+    }
+}
---
+++ Appium/src/test/java/com/Appium/StartAppium/BaseicTest.java
@@ -0,0 +1,56 @@
+package com.Appium.StartAppium;
+
+import org.testng.annotations.Test;
+
+import io.appium.java_client.android.AndroidDriver;
+import io.appium.java_client.android.options.UiAutomator2Options;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+
+import java.net.MalformedURLException;
+
+import java.net.URL;
+
+import org.testng.annotations.AfterClass;
+
+public class BaseicTest {
+  @Test
+  public void startDriver() throws MalformedURLException {
+	  
+	  AndroidDriver driver;
+	  URL appiumRunOn=new URL("http://127.0.0.1:4723");
+	  
+	  UiAutomator2Options option= new UiAutomator2Options();
+	  option.setDeviceName("Pixcel4a");
+	  option.setApp("D:\\AppiumWork\\Appium\\src\\test\\java\\resources\\ApiDemos-debug.apk");
+	  System.out.println(option.getPlatformName());
+	  System.out.println("Capabilities set");
+	  
+	  driver=new AndroidDriver(appiumRunOn, option);
+  }
+  @BeforeMethod
+  public void beforeMethod() {
+	  System.out.println("Startng the first test");
+  }
+
+  @AfterMethod
+  public void afterMethod() {
+
+	  System.out.println(" first end test");
+  }
+
+  @BeforeClass
+  public void beforeClass() {
+
+	  System.out.println("Startng execution of testsuite");
+  }
+
+  @AfterClass
+  public void afterClass() {
+
+	  System.out.println("End of execution if testsuite");
+  }
+
+}
